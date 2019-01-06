8bd0da5a4779945fa1197aaf052feadc05e4e94e
Sasank Vishnubhatla <sasank@vishnubhatlas.net>
2019-01-06 18:31:23 -0500

    working on creating good renders for github and personal website

--- analysis.R
+++ analysis.R
@@ -1,9 +1,5 @@
 library(rmarkdown)
 
-render("analysis.Rmd",
-	   output_file = "README.md",
-	   output_format = "md_document")
-
-render("analysis.Rmd",
-	   output_file = "analysis.html",
-	   output_format = "html_document")
+render("analysis.Rmd", output_format = "pdf_document", output_dir = "docs")
+render("analysis.Rmd", output_format = "html_document", output_dir = "docs")
+render("analysis.Rmd", output_format = "github_document", output_dir = "docs")
