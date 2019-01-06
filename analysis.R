45ae5ecb63f2d78fb8a12e6e0512c71c13eb0894
Sasank Vishnubhatla <sasank@vishnubhatlas.net>
2019-01-06 18:40:12 -0500

    still working on prelim renders

--- analysis.R
+++ analysis.R
@@ -2,4 +2,3 @@ library(rmarkdown)
 
 render("analysis.Rmd", output_format = "pdf_document", output_dir = "docs")
 render("analysis.Rmd", output_format = "html_document", output_dir = "docs")
-render("analysis.Rmd", output_format = "github_document", output_dir = "docs")
