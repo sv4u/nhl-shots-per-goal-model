3321789df43f65fb0fa4178f0854bb999c1e1a7d
Sasank Vishnubhatla <sasank@vishnubhatlas.net>
2019-01-06 18:13:55 -0500

    render 2 try

--- analysis.R
+++ analysis.R
@@ -1,3 +1,9 @@
 library(rmarkdown)
 
-render("analysis.Rmd", output_dir = "docs", output_format = "all")
+render("analysis.Rmd",
+	   output_file = "README.md",
+	   output_format = "md_document")
+
+render("analysis.Rmd",
+	   output_file = "analysis.html",
+	   output_format = "html_document")
