927a6f3f06b65629b69f220472f34bb386020b2f
Sasank Vishnubhatla <sasank@vishnubhatlas.net>
2019-01-07 22:13:41 -0500

    centers done

--- analysis.R
+++ analysis.R
@@ -1,4 +1,8 @@
 library(rmarkdown)
 
-render("analysis.Rmd", output_format = "pdf_document", output_dir = "docs")
-render("analysis.Rmd", output_format = "html_document", output_dir = "docs")
+render("analysis.Rmd",
+	   output_format = "pdf_document",
+	   output_dir = "docs")
+render("analysis.Rmd",
+	   output_format = "html_document",
+	   output_dir = "docs")
